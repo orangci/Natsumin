8885124be2cc22dcc6d29f1346d237a1efe030d9
orangci <orangc@proton.me>
2025-05-07 11:46:26 +0300

    feat: various changes

--- main.py
+++ main.py
@@ -1,40 +1,73 @@
 import discord
 from discord.ext import commands
-from config import BOT_CONFIG
+from config import BOT_CONFIG, BASE_EMBED_COLOR
 import os
 from dotenv import load_dotenv
 
+
 def clear():
-	if os.name == 'nt':
-		os.system("cls")
-	else:
-		os.system("clear")
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
 
 load_dotenv()
 bot = commands.Bot(
-	command_prefix=BOT_CONFIG.prefix,
-	status=discord.Status.online,
-	activity=discord.CustomActivity(name="?/? users passed | %%help"),
-	intents=discord.Intents.all(),
-	case_insensitive=True,
-	allowed_mentions=discord.AllowedMentions(
-		everyone=False, roles=False, users=True, replied_user=False
-	)
+    command_prefix=BOT_CONFIG.prefix,
+    status=discord.Status.online,
+    activity=discord.CustomActivity(name="?/? users passed | %help"),
+    intents=discord.Intents.all(),
+    case_insensitive=True,
+    allowed_mentions=discord.AllowedMentions(
+        everyone=False, roles=False, users=True, replied_user=False
+    ),
 )
+
 
 @bot.event
 async def on_ready():
-	clear()
-	print(f"Logged in as {bot.user.name}#{bot.user.discriminator}!")
+    clear()
+    print(f"Logged in as {bot.user.name}#{bot.user.discriminator}!")
+
 
 def recursive_load_cogs(path: str):
-	for file in os.listdir(path):
-		if file.endswith(".py") and not file.startswith("_"):
-			cog_name = f"{path.replace("/", ".")}.{file[0:-3]}"
-			bot.load_extension(cog_name)
-			continue
-		if os.path.isdir(f"{path}/{file}"):
-			recursive_load_cogs(f"{path}/{file}")
+    for file in os.listdir(path):
+        if file.endswith(".py") and not file.startswith("_"):
+            cog_name = f"{path.replace('/', '.')}.{file[0:-3]}"
+            bot.load_extension(cog_name)
+            continue
+        if os.path.isdir(f"{path}/{file}"):
+            recursive_load_cogs(f"{path}/{file}")
+
+
+class Help(commands.HelpCommand):
+    def get_command_signature(self, command):
+        return "%s%s %s" % (
+            self.context.clean_prefix,
+            command.qualified_name,
+            command.signature,
+        )
+
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title="Help", color=BASE_EMBED_COLOR)
+
+        for cog, cog_commands in mapping.items():
+            filtered = await self.filter_commands(cog_commands, sort=True)
+            command_signatures = [self.get_command_signature(c) for c in filtered]
+
+            if command_signatures:
+                cog_name = getattr(cog, "qualified_name", "No Category")
+                embed.add_field(
+                    name=cog_name, value="\n".join(command_signatures), inline=False
+                )
+
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
+
+bot.help_command = Help()
+
 
 recursive_load_cogs("cogs")
 bot.run(os.getenv("DISCORD_TOKEN"))

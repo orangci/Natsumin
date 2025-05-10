3be604dbbb3ed63bba4dc347a962c11414a20c7a
Richard <62023434+TrhRichard@users.noreply.github.com>
2025-05-11 00:07:28 +0300

    Remove "nice" messages
    While it was a cool thing the deadline seems way more important to show than a "nice" message, anyway the code wasnt even there in Contracts cog anymore so the bot was basically loading the json file for no reason

--- shared.py
+++ shared.py
@@ -1,28 +1,9 @@
-import json
-from typing import Optional, TypedDict
 import discord
 from discord.ext import commands
 
 
-def get_member_from_username(bot: commands.Bot, username: str) -> Optional[discord.Member]:
+def get_member_from_username(bot: commands.Bot, username: str) -> discord.Member | None:
 	for member in bot.get_all_members():
 		if member.name.lower() == username.lower():
 			return member
 	return None
-
-
-class NiceMessageCategory(TypedDict):
-	same_user: list[str]
-	different_user: list[str]
-	is_contractor: list[str]
-
-
-class NiceMessages(TypedDict):
-	finished: NiceMessageCategory
-	halfway: NiceMessageCategory
-	started: NiceMessageCategory
-	not_started: NiceMessageCategory
-
-
-with open("assets/nice_messages.json") as f:
-	nice_messages: NiceMessages = json.load(f)
